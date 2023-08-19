84c03a4a1b0f6a739a69c91b89dbc5efb492bc0d
belarmino <belarmino.veiga@gmail.com>
2023-08-19 11:23:10 -0100

    remove comments

--- Database/ReadPerson.cs
+++ Database/ReadPerson.cs
@@ -18,14 +18,11 @@ namespace EFCoreBufferingStreaming.Database
         public void ReadPersonBuffering()
         {
             List<Person> people = db.Person.AsNoTracking().ToList();
-            //int count = people.Count();
         }
         [Benchmark]
         public void ReadPersonStreaming()
         {
             IEnumerable<Person> people = db.Person.AsNoTracking();
-            //IEnumerable<Person> _people = people;
-            //int count = people.Count();
         }
     }
 }
